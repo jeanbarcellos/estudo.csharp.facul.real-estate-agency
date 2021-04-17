3adec2e0c194adc7eb82a37c16f4d47b6875f95e
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-17 20:38:08 -0300

    Domain.Tests - Melhorando os testes em Domain

--- tests/RealEstateAgency.Domain.Tests/Entities/ClientTests.cs
+++ tests/RealEstateAgency.Domain.Tests/Entities/ClientTests.cs
@@ -5,7 +5,7 @@ namespace RealEstateAgency.Domain.Tests.Entities
 {
     public class ClientTests
     {
-        [Fact(DisplayName = "Create Client")]
+        [Fact(DisplayName = "New client valid (simple)")]
         [Trait("Category", "Domain - Client")]
         public void Client_Create_ReturnClient()
         {
@@ -22,6 +22,44 @@ namespace RealEstateAgency.Domain.Tests.Entities
             Assert.Equal(socialNumber, client.SocialNumber);
             Assert.Equal(name, client.Name);
             Assert.Equal(birthday, client.Birthday);
+        }
+
+        [Fact(DisplayName = "New client valid")]
+        [Trait("Category", "Domain - Client")]
+        public void Client_NewClient_ShouldBeValid()
+        {
+            // Arrange
+            var socialNumber = "11122233389";
+            var name = "Jean Barcellos";
+            var birthday = new DateTime(1989, 12, 18);
+
+            var client = new Client(socialNumber, name, birthday);
+
+            // Act
+            var result = client.IsValid();
+
+            // Asssert
+            Assert.True(result);
+            Assert.Empty(client.ValidationResult.Errors);
+        }
+
+        [Fact(DisplayName = "New client invalid")]
+        [Trait("Category", "Domain - Client")]
+        public void Client_NewClient_ShouldBeInvalid()
+        {
+            // Arrange
+            var socialNumber = "";
+            var name = "";
+            var birthday = DateTime.Now;
+
+            var client = new Client(socialNumber, name, birthday);
+
+            // Act
+            var result = client.IsValid();
+
+            // Asssert
+            Assert.False(result);
+            Assert.NotEmpty(client.ValidationResult.Errors);
         }
     }
 }
