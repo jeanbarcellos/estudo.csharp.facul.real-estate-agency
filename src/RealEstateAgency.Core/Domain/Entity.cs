f0d935d77cd263f962e39e120aaec70f6db66230
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-17 17:14:23 -0300

    Criação do shared kernel

---
+++ src/RealEstateAgency.Core/Domain/Entity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RealEstateAgency.Core.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        #region BaseBehaviours
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (a is null && b is null)
+                return true;
+
+            if (a is null || b is null)
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() ^ 93) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+
+        #endregion
+    }
+}
