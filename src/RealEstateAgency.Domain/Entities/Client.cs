930a2ff68a276b6491d556988612dcce8eb3d74d
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-17 19:09:28 -0300

    Domain - Criação do IClientRepository

--- src/RealEstateAgency.Domain/Entities/Client.cs
+++ src/RealEstateAgency.Domain/Entities/Client.cs
@@ -3,10 +3,19 @@ using System;
 
 namespace RealEstateAgency.Domain
 {
-    public class Client : Entity
+    public class Client : Entity, IAggregateRoot
     {
         public string SocialNumber { get; set; }
         public string Name { get; set; }
         public DateTime Birthday { get; set; }
+
+        public Client(string socialNumber, string name, DateTime birthday)
+        {
+            SocialNumber = socialNumber;
+            Name = name;
+            Birthday = birthday;
+        }
     }
+
+
 }
---
+++ src/RealEstateAgency.Domain/Interfaces/IClientRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RealEstateAgency.Domain.Interfaces
+{
+    public interface IClientRepository
+    {
+        Task<IEnumerable<Client>> GetAll();
+        Task<Client> GetById(Guid id);
+
+        bool Insert(Client client);
+        bool Update(Client client);
+        bool Delete(Client client);
+        bool Delete(Guid id);
+    }
+}
