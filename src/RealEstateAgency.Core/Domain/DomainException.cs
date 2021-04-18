ffe5b12e8c3734b2a445a693595d600cce6a5043
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-17 22:04:17 -0300

    Domain - Adição de StringUtils

---
+++ src/RealEstateAgency.Core/Domain/DomainException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RealEstateAgency.Core.Domain
+{
+    public class DomainException : Exception
+    {
+        public DomainException()
+        { }
+
+        public DomainException(string message) : base(message)
+        { }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        { }
+    }
+}
