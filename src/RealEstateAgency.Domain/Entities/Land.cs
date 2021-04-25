b129712c440c763ee49cffd94246f19c290c9410
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-25 00:10:33 -0300

    Domain - Implementação de Land e suas validações

--- src/RealEstateAgency.Domain/Entities/Land.cs
+++ src/RealEstateAgency.Domain/Entities/Land.cs
@@ -1,9 +1,25 @@
-﻿namespace RealEstateAgency.Domain.Entities
+﻿using RealEstateAgency.Domain.Entities.Validations;
+
+namespace RealEstateAgency.Domain.Entities
 {
-    public class Land: Property
+    public class Land : Property
     {
         public bool OnACorner { get; set; }
         public decimal Width { get; set; }
         public decimal Height { get; set; }
+
+        public Land(bool onACorner, decimal width, decimal height, decimal saleValue, Client client) : base(saleValue, client)
+        {
+            OnACorner = onACorner;
+            Width = width;
+            Height = height;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new LandValidation().Validate(this);
+
+            return ValidationResult.IsValid;
+        }
     }
 }
---
+++ src/RealEstateAgency.Domain/Entities/Validations/LandValidation.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using System;
+
+namespace RealEstateAgency.Domain.Entities.Validations
+{
+    public class LandValidation : PropertyValidation<Land>
+    {
+        public static string OnACornerNotEmptyMessage = "Informação de o terreno é de esquina não informado";
+        public static string WidthNotEmptyMessage = "Largura do terreno não informada";
+        public static string HeightNotEmptyMessage = "Profundidade do terreno não informada";
+
+        public LandValidation()
+        {
+            ValiateOnACorner();
+            ValidateWidth();
+            ValidateHeight();
+            ValidateSaleValue();
+            ValidateClient();
+        }
+
+        private void ValiateOnACorner()
+        {
+            RuleFor(o => o.OnACorner)
+                .NotEmpty().WithMessage(OnACornerNotEmptyMessage);
+        }
+
+        private void ValidateWidth()
+        {
+            RuleFor(o => o.Width)
+                .NotEmpty().WithMessage(WidthNotEmptyMessage);
+        }
+
+        private void ValidateHeight()
+        {
+            RuleFor(o => o.Height)
+                .NotEmpty().WithMessage(HeightNotEmptyMessage);
+        }
+    }
+}
