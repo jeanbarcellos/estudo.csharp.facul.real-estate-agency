e77a695a308de31919449320d73585ad77de2691
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-25 22:47:33 -0300

    Core - Implementação das interfaces para Repository

---
+++ src/RealEstateAgency.Core/Data/IRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using RealEstateAgency.Core.Domain;
+using System;
+
+namespace RealEstateAgency.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
---
+++ src/RealEstateAgency.Core/Data/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace RealEstateAgency.Core.Data
+{
+    public interface IUnitOfWork
+    {
+        Task<bool> Commit();
+    }
+}
