009a9936aed356cd675037d896e411bc872fea5c
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-25 22:48:27 -0300

    Domain - Criação da interface IClientRepository

--- src/RealEstateAgency.Domain/Interfaces/IClientRepository.cs
+++ src/RealEstateAgency.Domain/Interfaces/IClientRepository.cs
@@ -1,17 +1,19 @@
-﻿using System;
+﻿using RealEstateAgency.Core.Data;
+using RealEstateAgency.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace RealEstateAgency.Domain.Interfaces
 {
-    public interface IClientRepository
+    public interface IClientRepository : IRepository<Client>
     {
         Task<IEnumerable<Client>> GetAll();
         Task<Client> GetById(Guid id);
 
-        bool Insert(Client client);
-        bool Update(Client client);
-        bool Delete(Client client);
-        bool Delete(Guid id);
+        void Insert(Client client);
+        void Update(Client client);
+        void Delete(Client client);
+        void Delete(Guid id);
     }
 }
