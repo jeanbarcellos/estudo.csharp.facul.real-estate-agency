f92f425f1b4c658ad82a65f877e3efcb2a8c30d4
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-29 21:00:57 -0300

    Criação do Pacote RealEstateAgency.Service.Api

---
+++ src/RealEstateAgency.Service.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateAgency.Service.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("Home");
+        }
+    }
+}
