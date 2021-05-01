488d5342531821663f92ce8ad21305a0f5331399
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-05-01 20:39:54 -0300

    Core - Criação de serializations

---
+++ src/RealEstateAgency.Core/Serialization/JsonDateConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RealEstateAgency.Core.Serialization
+{
+    public class JsonDateConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return DateTime.ParseExact(reader.GetString(), "yyyy-MM-dd", CultureInfo.InvariantCulture);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime dateTimeValue, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(dateTimeValue.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ src/RealEstateAgency.Core/Serialization/JsonNullableDateConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RealEstateAgency.Core.Serialization
+{
+    public class JsonNullableDateConverter : JsonConverter<DateTime?>
+    {
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.GetString() == "")
+                return null;
+            else
+                return DateTime.ParseExact(reader.GetString(), "yyyy-MM-dd", CultureInfo.InvariantCulture);
+        }
+
+        // This method will be ignored on serialization, and the default typeof(DateTime) converter is used instead.
+        // This is a bug: https://github.com/dotnet/corefx/issues/41070#issuecomment-560949493
+        public override void Write(Utf8JsonWriter writer, DateTime? dateTimeValue, JsonSerializerOptions options)
+        {
+            if (!dateTimeValue.HasValue)
+            {
+                writer.WriteStringValue("");
+            }
+            else
+            {
+                writer.WriteStringValue(dateTimeValue?.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
