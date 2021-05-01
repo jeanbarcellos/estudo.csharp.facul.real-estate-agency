d02b62bc26f4a2a36d5ebec0e8e91fc0b6a7a013
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-05-01 18:13:37 -0300

    Infra.Data - Nova migration Ajuste na tipagem das colunas

---
+++ src/RealEstateAgency.Infra.Data/Migrations/20210501210930_AjustementsToColumnsTypes.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RealEstateAgency.Infra.Data.Migrations
+{
+    public partial class AjustementsToColumnsTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_client",
+                table: "client");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "social_number",
+                table: "client",
+                type: "character varying(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(128)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "client",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(128)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "birthday",
+                table: "client",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "client_pk",
+                table: "client",
+                column: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "client_pk",
+                table: "client");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "social_number",
+                table: "client",
+                type: "varchar(128)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "client",
+                type: "varchar(128)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "birthday",
+                table: "client",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_client",
+                table: "client",
+                column: "id");
+        }
+    }
+}
