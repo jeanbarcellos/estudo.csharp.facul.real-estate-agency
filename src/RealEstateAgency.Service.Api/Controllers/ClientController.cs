26664e95cfccde263ec68883ce1bec1d6bfe29ed
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-05-01 22:15:16 -0300

    Service.Api - Finalização do CRUD em Client

--- src/RealEstateAgency.Service.Api/Controllers/ClientController.cs
+++ src/RealEstateAgency.Service.Api/Controllers/ClientController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using RealEstateAgency.Application.Service;
 using RealEstateAgency.Application.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RealEstateAgency.Service.Api.Controllers
@@ -31,6 +33,60 @@ namespace RealEstateAgency.Service.Api.Controllers
         public async Task<ClientViewModel> Show(Guid id)
         {
             return await _clientAppService.GetById(id);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Insert([FromBody] ClientViewModel clientViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _clientAppService.Add(clientViewModel);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] ClientViewModel clientViewModel)
+        {
+            if (clientViewModel == null || id == Guid.Empty || id != clientViewModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _clientAppService.Update(clientViewModel);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return NotFound();
+            }
+
+            var result = await _clientAppService.GetById(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            await _clientAppService.Delete(id);
+
+            return Ok(result);
         }
 
     }
