4e63e82a2fe81dfbf86f04340da90b0e74601bc7
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-05-07 00:37:48 -0300

    Service.Api - Criação de ViewModels para responstas padrão

---
+++ src/RealEstateAgency.Service.Api/ViewModels/ErrorResultViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RealEstateAgency.Service.Api.ViewModels
+{
+    public class ErrorResultViewModel
+    {
+        protected bool Success = false;
+        public string Message { get; set; }
+        public Dictionary<string, List<string>> Errors { get; set; }
+    }
+}
---
+++ src/RealEstateAgency.Service.Api/ViewModels/ValidationResultViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace RealEstateAgency.Service.Api.ViewModels
+{
+    public class ValidationResultViewModel
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public Dictionary<string, List<string>> Errors { get; private set; }
+
+        public ValidationResultViewModel()
+        {
+            Errors = new Dictionary<string, List<string>>();
+        }
+
+        public ValidationResultViewModel AddError(string key, string value)
+        {
+            var exists = Errors.ContainsKey(key);
+
+            if (exists)
+            {
+                Errors[key].Add(value);
+            }
+            else
+            {
+                Errors[key] = new List<string>
+                {
+                    value
+                };
+            }
+
+
+            return this;
+        }
+
+        internal void AddError(string key, List<string> errors)
+        {
+            Errors[key] = errors;
+        }
+    }
+}
